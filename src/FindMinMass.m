4438d5278f7510b7ac40169643a0ca276e08f7f6
Mahek Shah <mshah129@terpmail.umd.edu>
2025-09-15 17:37:16 -0400

    fixed array sizes again

--- src/FindMinMass.m
+++ src/FindMinMass.m
@@ -1,5 +1,5 @@
 function [minMos] = FindMinMass(Mos)
-    minMos=zeros(size(Mos));
+    minMos=zeros(1, length(Mos));
     for k = 1:length(Mos)
         [m,I]=min((Mos(k)));
         minMos(k)=[m,I]
