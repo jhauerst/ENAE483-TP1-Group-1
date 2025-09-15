bac81f504d5586a7e32b2ffe3cdd459bf9427c3e
AbdullahAlnamlah <alnamlah@terpmail.umd.edu>
2025-09-15 18:31:57 -0400

    code version.2

--- src/section1_1.m
+++ src/section1_1.m
@@ -1,39 +1,62 @@
 %% Section 1.1
+close all;
+clear;
+clc;
 %Givens
 delta_v = 12300; %m/s
 m_pl = 26000; %kg
 delta = 0.08; %Inert mass fraction for both stages
-chi = linspace(0.2,0.6,100); %array
+chi = 0.01:0.01:0.99; %array
 Isp1 = 366; %s (for LOX/LH2)
 Isp2 = 366; %s (for LOX/LH2)
 % Equations 
-[M01_array, M02_array] = getmass(delta_v,m_pl,delta,chi,Isp1,Isp2);
+
+[M01_array, M02_array, chi_array] = getmass(delta_v,m_pl,delta,chi,Isp1,Isp2);
 
 figure (1)
-plot(chi, M02_array)
+plot(chi_array, M02_array)
 hold on
 grid on
-plot(chi, M01_array)
-plot(chi,M01_array+M02_array)
+plot(chi_array, M01_array)
+plot(chi_array,M01_array+M02_array)
 legend 
 hold off
 
 
-function [M01_array, M02_array] = getmass(delta_v,m_pl,delta,chi,Isp1,Isp2)
+function [M01_array, M02_array, chi_array] = getmass(delta_v,m_pl,delta,chi,Isp1,Isp2)
     syms M02 M01
-    M02_array = zeros(size(chi));
-    M01_array = zeros(size(chi));
+    M02_array = [];
+    M01_array = [];
+    chi_array = [];
 
     for k = 1:length(chi)
         eqn2 = -Isp2*9.81*log((m_pl+delta*M02) / M02) - (1-chi(k))*delta_v == 0;
         soln2 = vpasolve(eqn2,M02);
-        M02_array(k) = double(soln2);
+        if soln2 > 0
+            try 
+                M02_array(end+1) = double(soln2);
+                chi_array(end+1) = [chi(k)];
+            catch
+            end
+        end
     end
 
-    for k = 1:length(chi)
-        eqn1 = -Isp1*9.81*log((M02_array(k)+delta*M01) / (M02_array(k)+M01)) - chi(k)*delta_v == 0;
-        soln1 = vpasolve(eqn1,M01)
-        M01_array(k) = double(soln1);
+    for k = 1:length(chi_array)
+        eqn1 = -Isp1*9.81*log((M02_array(k)+delta*M01) / (M02_array(k)+M01)) - chi_array(k)*delta_v == 0;
+        soln1 = vpasolve(eqn1,M01);
+        if soln1 > 0
+            try 
+                M01_array(end+1) = double(soln1);
+            catch
+            end
+        end
+    end
+    if length(M02_array) > length(M01_array)
+        chi_array = chi_array(1:length(M01_array));
+        M02_array = M02_array(1:length(M01_array));
+    elseif length(M01_array) > length(M02_array)
+        chi_array = chi_array(1:length(M02_array));
+        M01_array = M01_array(1:length(M02_array));
     end
 
 end
