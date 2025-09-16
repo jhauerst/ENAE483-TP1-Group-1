4cb3aad287f517ef2a4a9a16fcc7c7f5728189cb
AbdullahAlnamlah <alnamlah@terpmail.umd.edu>
2025-09-16 12:09:27 -0400

    version0.5
    I deleted the functions from the main code and put them in their own separate files.

--- src/section1_1.m
+++ src/section1_1.m
@@ -3,7 +3,7 @@ close all;
 clear;
 clc;
 %Givens
-delta_v = 12.3; %m/s
+delta_v = 12.3; %km/s
 m_pl = 26000; %kg
 delta = 0.08; %Inert mass fraction for both stages
 chi = 0.01:0.01:0.99; %array
@@ -24,8 +24,8 @@ hold off
 title('Total Masses vs. Chi')
 ylabel('Mass (kg)')
 xlabel('Chi')
-%% Functions
-%Get propellant mass
+
+
 [m_pr1, m_pr2] = propmass(delta,M01_array,M02_array, m_pl);
 figure(2)
 subplot(2,1,1)
@@ -41,57 +41,6 @@ ylabel('Mass (kg)')
 xlabel('Mass (kg)')
 grid on
 
-
-%Get initial masses of M01 & M02
-function [M01_array, M02_array, chi_array] = getmass(delta_v,m_pl,delta,chi,Isp1,Isp2)
-    syms M02 M01
-    M02_array = [];
-    M01_array = [];
-    chi_array = [];
-
-    for k = 1:length(chi)
-        g = 9.81/1000;
-        eqn2 = -Isp2*g*log((m_pl+delta*M02) / M02) - (1-chi(k))*delta_v == 0;
-        soln2 = vpasolve(eqn2,M02);
-        if soln2 > 0
-            try 
-                M02_array(end+1) = double(soln2);
-                chi_array(end+1) = [chi(k)];
-            catch
-            end
-        end
-    end
-
-    for k = 1:length(chi_array)
-        eqn1 = -Isp1*g*log((M02_array(k)+delta*M01) / (M02_array(k)+M01)) - chi_array(k)*delta_v == 0;
-        soln1 = vpasolve(eqn1,M01);
-        if soln1 > 0
-            try 
-                M01_array(end+1) = double(soln1);
-            catch
-            end
-        end
-    end
-    if length(M02_array) > length(M01_array)
-        chi_array = chi_array(1:length(M01_array));
-        M02_array = M02_array(1:length(M01_array));
-    elseif length(M01_array) > length(M02_array)
-        chi_array = chi_array(1:length(M02_array));
-        M01_array = M01_array(1:length(M02_array));
-    end
-
-end
-
-function [m_pr1, m_pr2] = propmass(delta,M01,M02, m_pl)
-    m_pr1 =[];
-    m_pr2 = [];
-    for k = 1:length(M01)
-        m_in1 = delta*M01(k);
-        m_pr1(k) = M01(k) - m_in1;
-        m_in2 = delta*M02(k);
-        m_pr2(k) = M02(k) - m_pl - m_in2;
-    end
-end
 
 
         
