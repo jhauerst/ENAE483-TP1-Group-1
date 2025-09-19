cc7716b3c68998873dc06077188ed164aea0ab13
eperez14 <eperez14@terpmail.umd.edu>
2025-09-19 19:01:57 -0400

    Update propMass.m
    added comments

--- src/propMass.m
+++ src/propMass.m
@@ -1,11 +1,11 @@
 %Get propellant mass
 function [m_pr1, m_pr2] = propMass(delta,M01,M02, m_pl)
-    m_pr1 =[];
+    m_pr1 =[]; % establish array
     m_pr2 = [];
-    for k = 1:length(M01)
-        m_in1 = delta*M01(k);
-        m_pr1(k) = M01(k) - m_in1;
-        m_in2 = delta*M02(k);
-        m_pr2(k) = M02(k) - m_pl - m_in2;
+    for k = 1:length(M01) % for length of stage 1 total mass array
+        m_in1 = delta*M01(k); % inert mass formula for stage 1
+        m_pr1(k) = M01(k) - m_in1; % mass of propellant stage 1
+        m_in2 = delta*M02(k); % inert mass formula for stage 2
+        m_pr2(k) = M02(k) - m_pl - m_in2; % mass of propellant stage 2
     end
 end
