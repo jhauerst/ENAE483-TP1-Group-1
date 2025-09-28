d6649357efc0c194dde2f64413d60ca75e2a9892
eperez14 <eperez14@terpmail.umd.edu>
2025-09-28 16:16:01 -0400

    Update findFairingMass.m
    added 3 m to Aft fairing

--- src/findFairingMass.m
+++ src/findFairingMass.m
@@ -1,7 +1,7 @@
 function [Payload, Intertank, Interstage, Aft] = findFairingMass(r, rf1, rf2,h)
 A_cone = pi*r*sqrt(r^2 + h^2);
 A_frustrum = pi*(rf1 + rf2)*sqrt((rf1-rf2)^2+h^2);
-A_cylinder = 2*pi*r*h;
+A_cylinder = 2*pi*r*(h+3); % leave sufficient engine fairing below propellant tanks (3m)
 Payload = 4.95*(A_cone)^1.15;
 Intertank = 4.95*(A_frustrum)^1.15;
 Interstage = 4.95*(A_frustrum)^1.15;
