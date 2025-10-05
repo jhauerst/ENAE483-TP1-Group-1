9c7b9f97bb75421821990d37c1d9476f81e078ce
AbdullahAlnamlah <alnamlah@terpmail.umd.edu>
2025-10-05 17:17:58 -0400

    Changes to findfuelmass function

--- src/totalInertMass.m
+++ src/totalInertMass.m
@@ -1,13 +1,21 @@
 %% Array of givens
-%array = [stage height, radius of stage, radius of fuel tank, 
-% radius of oxidizer tank, ]
-array1 = [h1, r, rt1, rt2]; 
-array2 = [];
+%array = [1:stage height, 2:radius of stage, 3:radius of fuel tank, 
+% 4:radius of oxidizer tank, 5:Propellant mass, 6:fuel fraction, 
+% 7:oxidizer fraction, ]
+%Names = [1:stage 1 fuel name, 2:stage 1 oxidizer name]
+stage1 = [h1, r, rt1, rt2]; 
+stage2 = [];
+names = [];
 
-function [mass_total_inert]=totalInertMass(array1,array2)
+function [mass_total_inert]=totalInertMass(stage1,stage2,names)
     %% Stage one
     %Fairing mass
-    [~, Intertank1, Aft1] = findFairingMass(array1(2), array1(3), array1(4),array1(1));
+    [~, Intertank1, Aft1] = findFairingMass(stage1(2), stage1(3), stage1(4),stage1(1));
+    %Fuel mass & Oxidizer masses
+    [Moxidizer, Mfuel] = findFuelMass(stage1(5), [stage1(6), stage1(7)]);
+    %Find volume of fuel & oxidizer 
+    fuel_tank = findTankMass(Mfuel,names(1),V_fuel);
+    oxidizer_tank = findTankMass(Moxidizer,names(2),V_oxz);
     %AT=2*pi*rf1*h+2*pi*rf1^2;
     
     mass_total_inert = TotalFairingMass;
