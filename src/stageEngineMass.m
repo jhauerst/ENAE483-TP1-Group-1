2ede0bab777a63f7f8003a4b216971e2fe4baf84
AbdullahAlnamlah <alnamlah@terpmail.umd.edu>
2025-10-05 18:18:37 -0400

    changes to stageEngineMass

--- src/stageEngineMass.m
+++ src/stageEngineMass.m
@@ -1,14 +1,45 @@
-function [engineMass, structureMass, gimbalsMass] = stageEngineMass(m0, mProp, TWR, P0, expanRatio, nEngines, type)
+function [engineMass, structureMass, gimbalsMass] = stageEngineMass(m0, mProp, TWR, type, nEngine, stageNum)
+    %m0: total mass per stage (kg)
+    %mprop: propellant mass per stage (kg)
+    %TWR: thrust to weight ratio
+    %Type: propellant name (fuel/oxidizer)
+    %nEngine: number of engines
+    %stageNum: stage number 
     g = 9.81;
-    Treq = m0 * g * TWR/nEngines;
-
-    structureMass = 2.55*10^-4*Treq;
-    gimbalsMass = 237.8*(Treq/P0)^0.9375;
+    propNames = {'LOX/LCH4','LOX/LH2','LOX/RP1','Solid','Storables'};
+    % arrays of thrust, chamber pressure, and expansion ratios per stage per
+    % propellant
+    s1Thrust = [2.26e+6, 1.86e+6, 1.92e+6, 4.5e+6, 1.75e+6]; 
+    s2Thrust = [0.745e+6, 0.099e+6, 0.061e+6, 2.94e+6, 0.067e+6];
+    s1Pressure = [35.16e+6, 20.64e+6, 25.8e+6, 10.5e+6, 15.7e+6];
+    s2Pressure = [10.1e+6, 4.2e+6, 6.77e+6, 5e+6, 14.7e+6];
+    s1ExpantionRatio = [34.34, 78, 37, 16, 26.2];
+    s2ExpantionRatio = [45, 84, 14.5, 56, 81.3];
+    %Loop through propellant names and find values based on propellant pick
+    for i = 1:length(propNames)
+        if strcmp(type, propNames(i))
+            if stageNum == 1
+                TPerEngine = s1Thrust(i);
+                P0 = s1Pressure(i);
+                expanRatio = s1ExpantionRatio(i);
+            end
+            if stageNum == 2
+                TPerEngine = s2Thrust(2);
+                P0 = s2Pressure(i);
+                expanRatio = s2ExpantionRatio(i);
+            end
+        end
+    end
+    T_total = TWR * m0 * g;%Total thrust (N)
+    structureMass = 2.55*10^-4*T_total;%overall structure mass
+    gimbalsMass = nEngine(237.8*(TPerEngine/P0)^0.9375);%Gimbal mass per engine
 
     if strcmp(type, "LOX/LH2") || strcmp(type, "LOX/LCH4") || strcmp(type, "LOX/RP1") || strcmp(type, "Storables")
-        engineMass = 7.81*10^-4*Treq + 3.37*10^-5*Treq*sqrt(expanRatio) + 59;
+        engineMass = 7.81*10^-4*TPerEngine + 3.37*10^-5*TPerEngine*sqrt(expanRatio) + 59;% Engine mass
     end
     if strcmp(type, "Solid")
         engineMass = 0.135*mProp;
     end
+    engineMass = nEngine*engineMass;
+
 end
--- src/totalInertMass.m
+++ src/totalInertMass.m
@@ -1,7 +1,8 @@
 %% Array of givens
 %array = [1:stage height, 2:radius of stage, 3:radius of fuel tank, 
 % 4:radius of oxidizer tank, 5:Propellant mass, 6:fuel fraction, 
-% 7:oxidizer fraction, ]
+% 7:oxidizer fraction, 8:fuel density, 9:oxidizer density ]
+
 %Names = [1:stage 1 fuel name, 2:stage 1 oxidizer name]
 stage1 = [h1, r, rt1, rt2]; 
 stage2 = [];
@@ -12,11 +13,22 @@ function [mass_total_inert]=totalInertMass(stage1,stage2,names)
     %Fairing mass
     [~, Intertank1, Aft1] = findFairingMass(stage1(2), stage1(3), stage1(4),stage1(1));
     %Fuel mass & Oxidizer masses
-    [Moxidizer, Mfuel] = findFuelMass(stage1(5), [stage1(6), stage1(7)]);
+    %Pass propellant mass and ratio
+    [Moxidizer1, Mfuel1] = findFuelMass(stage1(5), [stage1(6), stage1(7)]);
     %Find volume of fuel & oxidizer 
-    fuel_tank = findTankMass(Mfuel,names(1),V_fuel);
-    oxidizer_tank = findTankMass(Moxidizer,names(2),V_oxz);
-    %AT=2*pi*rf1*h+2*pi*rf1^2;
+    V_fuel1 = findVolume(Mfuel1,stage1(8));%Pass fuel density
+    V_oxid1 = findVolume(Moxidizer1,stage1(9));%Pass oxidizer density
+    %Find tanks masses
+    fuel_tank1 = findTankMass(Mfuel1,names(1),V_fuel1);
+    oxidizer_tank1 = findTankMass(Moxidizer1,names(2),V_oxid1);
+    %find Surface area of tanks
+    h_fuel1 =findTankHeight(Mfuel1,stage1(3),stage1(8)); %height of fuel tank
+    h_oxid1 =findTankHeight(Moxidizer1,stage1(4),stage1(9)); %height of oxidizer tank
+    Asurf_fuel1 = 2*pi*stage1(3)*h_fuel1 +4*pi*stage1(3)^2;%surface area of fuel tank
+    Asurf_oxidizer1 = 2*pi*stage1(4)*h_oxid1 +4*pi*stage1(4)^2;%surface area of oxidizer tank
+    insul_fuel1 = findInsulationMass(Asurf_fuel1,names(1)); %insulaiton mass for fuel tank
+    insul_oxid1 = findInsulationMass(Asurf_oxidizer1,names(2)); %insulation mass for oxidizer tank
+    % find 
     
     mass_total_inert = TotalFairingMass;
     %% Stage two
