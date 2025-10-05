8e88c1aea5cd9f251d2bd3b73296abf723bd45a9
AbdullahAlnamlah <alnamlah@terpmail.umd.edu>
2025-10-05 16:45:40 -0400

    Changes to fairing mass function

--- src/totalInertMass.m
+++ src/totalInertMass.m
@@ -1,6 +1,15 @@
-function [mass_total_inert]=totalInertMass(rf1,rf2,h,fuelname)
-    [~, ~, ~, ~,TotalFairingMass] = findFairingMass(r, rf1, rf2,h);
-    AT=2*pi*rf1*h+2*pi*rf1^2;
-    [InsMass] = findInsulationMass(AT,fuelname);
+%% Array of givens
+%array = [stage height, radius of stage, radius of fuel tank, 
+% radius of oxidizer tank, ]
+array1 = [h1, r, rt1, rt2]; 
+array2 = [];
+
+function [mass_total_inert]=totalInertMass(array1,array2)
+    %% Stage one
+    %Fairing mass
+    [~, Intertank1, Aft1] = findFairingMass(array1(2), array1(3), array1(4),array1(1));
+    %AT=2*pi*rf1*h+2*pi*rf1^2;
+    
     mass_total_inert = TotalFairingMass;
+    %% Stage two
 end
