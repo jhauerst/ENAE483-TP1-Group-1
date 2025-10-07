313bd5b647972a601e67431245855a555f4a74ff
jhauerst <jhauerst@terpmail.umd.edu>
2025-10-07 11:30:54 -0400

    update insulation mass calcs

--- src/findInsulationMass.m
+++ src/findInsulationMass.m
@@ -2,8 +2,10 @@
 function InsMass = findInsulationMass(AT,fuelname) %exposed surface area of tank, fuel name
     if strcmp(fuelname, "LH2")
         InsMass = 2.88*AT; 
-    else 
+    elseif strcmp(fuelname, "LCH4") || strcmp(fuelname, "LOX")
         InsMass = 1.123*AT;
+    else
+        InsMass = 0;
     end
 end
 
